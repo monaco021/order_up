08724643aafb404bbfb4642ce0a30d3bc1bec5c1
Erick Bravo <erickbrvo@gmail.com>
2021-01-13 15:27:22 -0800

    create db mgmt start

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from enum import unique
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from sqlalchemy.schema import Column
+from sqlalchemy.types import Integer, String
 from werkzeug.security import generate_password_hash, check_password_hash #bcrypt equiv
 
 db = SQLAlchemy()
