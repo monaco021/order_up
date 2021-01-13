3b358ba577b4ee2c887172206d9fbe43fb132429
james <monaco.james18@gmail.com>
2021-01-13 16:24:38 -0600

    login page

---
+++ app/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    employee_number = StringField("Employee number", [DataRequired()])
+    password = PasswordField("Password", [DataRequired()])
+    submit = SubmitField("Login")
